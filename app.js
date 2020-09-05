e5a68747f91865eaf037d72e4d3dc1e3d14a8071
Marco Tamayo Flores <marco.com17@gmail.com>
2020-09-05 18:03:28 -0500

    Base de datos reconfigurado

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog area of site"
 
 //Para comprimir respuestas http
-var compression = require('compression'); //Se aplica solo en producción
-var helmet = require('helmet'); //Puede arreglar encabezados para proteger de amenazas conocidas. Se hace en entorno de producción
+//var compression = require('compression'); //Se aplica solo en producción
+//var helmet = require('helmet'); //Puede arreglar encabezados para proteger de amenazas conocidas. Se hace en entorno de producción
 
 var app = express();
 
-app.use(helmet());
+//app.use(helmet());
 
 //Configurando la base de datos
 var mongoose = require('mongoose');
 //var mongoDb = 'mongodb://lib:12345678@localhost:27017/libreria';
-var dev_db_url = 'mongodb+srv://lib:12345678fe15cd45454b.ngrok.io/libreriay?retryWrites=true';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var dev_db_url = 'mongodb://lib:12345678@2.tcp.ngrok.io:19556/libreria?retryWrites=true';
+var mongoDb = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDb, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
@@ -38,7 +38,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compression()); //Se comprimen todas las rutas
+//app.use(compression()); //Se comprimen todas las rutas
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
